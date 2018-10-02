7fb745b03ecb7111f316e99e34b199aa9f8923ee
zpf <it_panda@163.com>
2018-10-03 01:12:40 +0800

    update

--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -2,43 +2,48 @@
   <section class="loginContainer">
     <div class="loginInner">
       <div class="login_header">
-        <h2 class="login_logo">硅谷外卖</h2>
+        <h2 class="login_logo">麓谷外卖</h2>
         <div class="login_header_title">
-          <a href="javascript:;" class="on">短信登录</a>
-          <a href="javascript:;" >密码登录</a>
+          <a href="javascript:;" :class="{on:loginWay}" @click="loginWay=true">短信登录</a>
+          <a href="javascript:;" :class="{on:!loginWay}" @click="loginWay=false">密码登录</a>
         </div>
       </div>
       <div class="login_content">
         <form>
-          <div class="on">
+          <div :class="{on:loginWay}">
             <section class="login_message">
-              <input type="tel" maxlength="11" placeholder="手机号">
-              <button disabled="disabled" class="get_verification" >获取验证码</button>
+              <input type="tel" maxlength="11" placeholder="手机号" v-model="phone">
+              <button class="get_verification"
+                      :disabled="sendCodeTime>0 || !isPhoneNumber"
+                      :class="{right_phone_number:isPhoneNumber}"
+                      @click.prevent="sendCode"
+              >
+                {{sendCodeTime ? `已发送${sendCodeTime}s` : '获取验证码'}}
+              </button>
             </section>
             <section class="login_verification">
-              <input type="tel" maxlength="8" placeholder="验证码">
+              <input type="tel" maxlength="8" placeholder="验证码" v-model="code">
             </section>
             <section class="login_hint">
-              温馨提示：未注册硅谷外卖帐号的手机号，登录时将自动注册，且代表已同意
+              温馨提示：未注册麓谷外卖帐号的手机号，登录时将自动注册，且代表已同意
               <a href="javascript:;">《用户服务协议》</a>
             </section>
           </div>
-          <div>
+          <div :class="{on:!loginWay}">
             <section>
               <section class="login_message">
-                <input type="tel" maxlength="11" placeholder="手机/邮箱/用户名">
+                <input type="tel" maxlength="11" placeholder="手机/邮箱/用户名" v-model="name">
               </section>
               <section class="login_verification">
-                <input type="tel" maxlength="8" placeholder="密码">
-                <div class="switch_button off">
-                  <div class="switch_circle"></div>
-                  <span class="switch_text">...</span>
+                <input :type="showPwd ? 'tel' : 'password'" maxlength="8" placeholder="密码" v-model="pwd">
+                <div class="switch_button" :class="showPwd ? 'on' : 'off'" @click="showPwd = !showPwd">
+                  <div class="switch_circle" :class="{right:showPwd}"></div>
+                  <span class="switch_text">{{showPwd ? 'on' : 'off'}}</span>
                 </div>
               </section>
               <section class="login_message">
-                <input type="text" maxlength="11" placeholder="验证码">
-                <img class="get_veri
-                fication" src="./images/captcha.svg" alt="captcha">
+                <input type="text" maxlength="11" placeholder="验证码" v-model="captcha">
+                <img class="get_verification" src="http://localhost:3000/captcha" alt="captcha" @click="updateCaptcha">
               </section>
             </section>
           </div>
@@ -56,6 +61,39 @@
 <script>
 	import './Login.styl'
 	export default {
-	  name: 'Login'
+	  name: 'Login',
+    data(){
+	    return {
+	      loginWay:true,
+        code:'',
+        phone:'',
+        name:'',
+        pwd:'',
+        captcha:'',
+        sendCodeTime:0,
+        showPwd:false
+      };
+    },
+    computed:{
+	    isPhoneNumber(){
+	      return /^1\d{10}$/g.test(this.phone);
+      }
+    },
+    methods:{
+	    sendCode(){
+        if(this.isPhoneNumber){
+          this.sendCodeTime = 30;
+          const timer = setInterval(() => {
+            this.sendCodeTime--;
+            if(this.sendCodeTime == 0){
+              clearInterval(timer);
+            }
+          },1000);
+        }
+      },
+      updateCaptcha(ev){
+	      ev.target.src = `http://localhost:3000/captcha?t=${new Date().getTime()}`;
+      }
+    }
 	}
 </script>
